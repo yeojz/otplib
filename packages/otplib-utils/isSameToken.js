0668eefa1df6445fcc51aa1b9f3fa3cc16c6385b
Gerald Yeo <gerald@fusedthought.com>
2018-07-20 22:15:21 +0800

    fix: non-int appended strings parsed as int

--- packages/otplib-utils/isSameToken.js
+++ packages/otplib-utils/isSameToken.js
@@ -1,13 +1,21 @@
+function isValidToken(value) {
+  return /^(\d+)(\.\d+)?$/.test(value);
+}
+
 /**
  * Simple comparison of 2 tokens
  *
  * @module otplib-utils/isSameToken
- * @param {string} token1 - base value
- * @param {string} token2 - value to compare
+ * @param {string | number} token1 - base value
+ * @param {string | number} token2 - value to compare
  * @return {boolean}
  */
 function isSameToken(token1, token2) {
-  return parseFloat(token1) === parseFloat(token2);
+  if (isValidToken(token1) && isValidToken(token2)) {
+    return String(token1) === String(token2);
+  }
+
+  return false;
 }
 
 export default isSameToken;
