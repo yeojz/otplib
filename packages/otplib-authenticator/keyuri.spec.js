6adaa080d016dc2eac785bb862fe9d69df04b53c
Tony Brix <tony@brix.ninja>
2019-07-25 09:57:32 -0500

    add tests

--- packages/otplib-authenticator/keyuri.spec.js
+++ packages/otplib-authenticator/keyuri.spec.js
@@ -1,20 +1,43 @@
 import keyuri from './keyuri';
 
 describe('keyuri', () => {
+  const opts = {
+    algorithm: 'sha1',
+    digits: 6,
+    step: 30
+  };
   [
-    ['otpauth://totp/test:me?secret=123&issuer=test', 'me', 'test', '123'],
-    ['otpauth://totp/null:null?secret=null&issuer=null', null, null, null],
     [
-      'otpauth://totp/service:user?secret=&issuer=service',
-      void 0,
-      void 0,
-      void 0
+      'otpauth://totp/test:me?secret=123&issuer=test&algorithm=sha1&digits=6&period=30',
+      'me',
+      'test',
+      '123',
+      opts
     ],
     [
-      'otpauth://totp/test%20got%20space:me%20got%20space?secret=123&issuer=test%20got%20space',
+      'otpauth://totp/null:null?secret=null&issuer=null&algorithm=sha1&digits=6&period=30',
+      null,
+      null,
+      null,
+      opts
+    ],
+    [
+      'otpauth://totp/test%20got%20space:me%20got%20space?secret=123&issuer=test%20got%20space&algorithm=sha1&digits=6&period=30',
       'me got space',
       'test got space',
-      '123'
+      '123',
+      opts
+    ],
+    [
+      'otpauth://totp/test:me?secret=123&issuer=test&algorithm=alg&digits=1&period=2',
+      'me',
+      'test',
+      '123',
+      {
+        algorithm: 'alg',
+        digits: 1,
+        step: 2
+      }
     ]
   ].forEach((entry, idx) => {
     const [url, ...args] = entry;
