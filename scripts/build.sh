0a0644046fe773626f513613380cd7b3a6786738
Gerald Yeo <gerald@fusedthought.com>
2019-08-25 00:07:10 +0800

    chore: github ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,25 +1,4 @@
-echo "\n--- cleaning prev builds ---"
-npx rimraf \
-  builds/otplib
-
-echo "\n--- building typedef ---"
-npx tsc \
-  --emitDeclarationOnly \
-  -p ./configs/tsconfig.json
-
-echo "\n--- modifying file paths ---"
-node ./scripts/renameDir.js
-node ./scripts/updateContent \
-  typedef \
-  ./builds/otplib/**/*.d.ts
-
-echo "\n--- build modules ---"
-NODE_ENV=production npx rollup \
-  -c ./configs/rollup.config.js
-
-echo "\n--- build bundles ---"
-NODE_ENV=production npx webpack \
-  --config ./configs/webpack.config.js
+./scripts/build-ci.sh
 
 echo "\n--- downloading buffer module ---"
 curl https://bundle.run/buffer@5.3.0 \
