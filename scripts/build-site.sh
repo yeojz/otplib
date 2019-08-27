b3b2fcec2fa78ea2179de39267af00f8aefa8f99
Gerald Yeo <gerald@fusedthought.com>
2019-08-27 22:21:08 +0800

    ci: create artifacts, consolidate dev builds

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 if [ "$OTPLIB_BUILD_SITE_PACKAGE" == "true" ]; then
-  echo "\n--- installing and build npm package ---"
+  echo "--- installing and build npm package ---"
   OTPLIB_SETUP_EXTRAS=skip \
-    OTPLIB_BUILD_REINSTALL=true \
-    OTPLIB_BUILD_CLEAN=true \
-    OTPLIB_BUILD_MODULE=false \
-    OTPLIB_BUILD_BUNDLE=true \
-    OTPLIB_BUILD_INCLUDE_BUFFER=true \
-    OTPLIB_BUILD_COPY_META=false \
-    npm run build
+  npm run setup
+
+  OTPLIB_BUILD_CLEAN=true \
+  OTPLIB_BUILD_MODULE=false \
+  OTPLIB_BUILD_BUNDLE=true \
+  OTPLIB_BUILD_INCLUDE_BUFFER=true \
+  OTPLIB_BUILD_COPY_META=false \
+  npm run build
 fi
 
-echo "\n--- cleaning prev site builds ---"
+echo "--- cleaning prev site builds ---"
 npx rimraf \
   ./builds/typedocs \
   ./website/static/otplib-browser \
@@ -20,23 +21,23 @@ npx rimraf \
   ./website/static/api \
   ./website/public
 
-echo "\n--- copying bundle to website ---"
+echo "--- copying bundle to website ---"
 cp -r ./builds/otplib/preset-browser/. ./website/static/otplib-browser
 
-echo "\n--- building api docs ---"
+echo "--- building api docs ---"
 npx typedoc \
   --hideGenerator \
   --options ./configs/typedoc.json \
   --out ./builds/typedocs
 
-echo "\n--- copying api docs to website ---"
+echo "--- copying api docs to website ---"
 cp -r ./builds/typedocs/. ./website/static/api
 
-echo "\n--- changing to website folder ---"
+echo "--- changing to website folder ---"
 cd website
 
 if [ "$OTPLIB_BUILD_SITE_REINSTALL" == "true" ]; then
-  echo "\n--- install website node_modules ---"
+  echo "--- install website node_modules ---"
   npm ci;
 fi
 
