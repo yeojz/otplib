4afbd67f6c6c15336b313188caacdacc942c237b
Ilja <rauchboy@gmail.com>
2020-06-11 13:35:17 +0300

    Update correct links

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -8,9 +8,9 @@ import styles from './index.module.css';
 const Hero = () => (
   <h2 className={styles.hero}>
     otplib is a pluggable JavaScript One-Time Password (OTP) library with
-    support for <a href="/api/classes/hotp.html">HOTP</a>,{' '}
-    <a href="/api/classes/totp.html">TOTP</a> and{' '}
-    <a href="/api/classes/authenticator.html">Google Authenticator</a>.
+    support for <a href="https://github.com/yeojz/otplib/blob/master/README.md#hotp-options">HOTP</a>,{' '}
+    <a href="https://github.com/yeojz/otplib/blob/master/README.md#totp-options">TOTP</a> and{' '}
+    <a href="https://github.com/yeojz/otplib/blob/master/README.md#google-authenticator">Google Authenticator</a>.
   </h2>
 );
 
