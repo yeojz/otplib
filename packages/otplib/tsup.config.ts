4ac7616ed77b9028642d3f86ac06aab2dcea643e
Gerald Yeo <me@yeojz.dev>
2026-02-27 18:31:49 +0800

    feat: add iife support

--- packages/otplib/tsup.config.ts
+++ packages/otplib/tsup.config.ts
@@ -2,19 +2,32 @@ import { defineConfig } from "tsup";
 
 const isProduction = process.env.NODE_ENV === "production" || process.env.CI === "true";
 
-export default defineConfig({
-  entry: {
-    index: "src/index.ts",
-    functional: "src/functional.ts",
-    class: "src/class.ts",
+export default defineConfig([
+  {
+    entry: {
+      index: "src/index.ts",
+      functional: "src/functional.ts",
+      class: "src/class.ts",
+    },
+    format: ["cjs", "esm"],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    target: "es2022",
+    tsconfig: "./tsconfig.json",
+    minify: isProduction,
+    metafile: true,
   },
-  format: ["cjs", "esm"],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  target: "es2022",
-  tsconfig: "./tsconfig.json",
-  minify: isProduction,
-  metafile: true,
-});
+  {
+    entry: { index: "src/index.ts" },
+    format: ["iife"],
+    globalName: "otplib",
+    noExternal: [/(.*)/],
+    splitting: false,
+    sourcemap: true,
+    target: "es2022",
+    minify: true,
+    metafile: true,
+  },
+]);
